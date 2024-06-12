010f667776d19f16a5561dbab78206f15979301f
Yerassyl <zhanymkanov@gmail.com>
2024-06-13 04:40:41 +0500

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+from pydantic import PostgresDsn, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from src.constants import Environment
+
+
+class CustomBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+class Config(CustomBaseSettings):
+    DATABASE_URL: PostgresDsn
+
+    SITE_DOMAIN: str = "myapp.com"
+
+    ENVIRONMENT: Environment = Environment.PRODUCTION
+
+    SENTRY_DSN: str | None = None
+
+    CORS_ORIGINS: list[str]
+    CORS_ORIGINS_REGEX: str | None = None
+    CORS_HEADERS: list[str]
+
+    APP_VERSION: str = "0.1"
+
+    @model_validator(mode="after")
+    def validate_sentry_non_local(self) -> "Config":
+        if self.ENVIRONMENT.is_deployed and not self.SENTRY_DSN:
+            raise ValueError("Sentry is not set")
+
+        return self
+
+
+settings = Config()
+
+app_configs: dict[str, Any] = {"title": "App API"}
+if settings.ENVIRONMENT.is_deployed:
+    app_configs["root_path"] = f"/v{settings.APP_VERSION}"
+
+if not settings.ENVIRONMENT.is_debug:
+    app_configs["openapi_url"] = None  # hide docs
