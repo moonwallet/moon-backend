477c705b15fe511cc90c2dbf06aca7e4f4a274ff
Yerassyl Zhanymkanov <zhanymkanov@gmail.com>
2024-06-14 23:46:59 +0300

    Update app configs

--- src/config.py
+++ src/config.py
@@ -33,9 +33,4 @@ class Config(CustomBaseSettings):
 
 settings = Config()
 
-app_configs: dict[str, Any] = {"title": "App API"}
-if settings.ENVIRONMENT.is_deployed:
-    app_configs["root_path"] = f"/v{settings.APP_VERSION}"
-
-if not settings.ENVIRONMENT.is_debug:
-    app_configs["openapi_url"] = None  # hide docs
+app_configs: dict[str, Any] = {"title": "Moon API"}
