de5bc6f8ceee2007e0861690e21ec285019aaaf2
Yerassyl Zhanymkanov <zhanymkanov@gmail.com>
2024-06-19 11:07:19 +0300

    Fix db connection

--- src/config.py
+++ src/config.py
@@ -13,6 +13,11 @@ class CustomBaseSettings(BaseSettings):
 class Config(CustomBaseSettings):
     DATABASE_URL: PostgresDsn
     DATABASE_ASYNC_URL: PostgresDsn
+    DATABASE_POOL_SIZE: int = 16
+    DATABASE_POOL_MAX_OVERFLOW: int = 10
+    DATABASE_POOL_TTL: int = 60 * 20  # 20 minutes
+    DATABASE_POOL_PRE_PING: bool = True
+
     REDIS_URL: RedisDsn
     REDIS_CONNECTION_POOL_SIZE: int = 16
 
