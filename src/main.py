fb34f65f59aad5980c237fd5e36da3f0ef21a2ba
Yerassyl Zhanymkanov <zhanymkanov@gmail.com>
2024-06-21 01:01:08 +0300

    Add sentry profile rate

--- src/main.py
+++ src/main.py
@@ -39,6 +39,7 @@ if settings.ENVIRONMENT.is_deployed:
     sentry_sdk.init(
         dsn=settings.SENTRY_DSN,
         environment=settings.ENVIRONMENT,
+        profiles_sample_rate=0.2,
     )
 
 
